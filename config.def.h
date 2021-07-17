8d7e1ab6de8255e64cb1e1c92304478102364131
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-07-16 18:59:54 -0700

    default fonts

--- config.def.h
+++ config.def.h
@@ -5,11 +5,20 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+
+/* static char *font = "Iosevka Nerd Font Mono:pixelsize=16:antialias=true:autohint=true"; */
+/* static char *font = "JetBrainsMono Nerd Font Mono:pixelsize=20:antialias=true:autohint=true"; */
+/* static char *font = "Noto Sans Mono:pixelsize=20:antialias=true:autohint=true"; */
+/* static char *font = "SauceCodePro Nerd Font Mono:pixelsize=20:antialias=true:autohint=true"; */
+/* static char *font = "Terminus:pixelsize=20:antialias=true:autohint=true"; */
+/* static char *font = "scientifica:style=Regular:pixelsize=22:antialias=false:autohint=true"; */
+static char *font = "UbuntuMono Nerd Font:pixelsize=20:antialias=true:autohint=true";
+
 /* Spare fonts */
 static char *font2[] = {
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+	"IPAGothic:pixelsize=20:antialias=true:autohint=true",
+	"Symbola:pixelsize=20:antialias=true:autohint=true",
+	"Braille:pixelsize=20:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
